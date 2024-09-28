a677f1d9dfe7b4a24e79c980d3bc1a136e2f118e
Ehsan Azizyan <ehsanazizyan74@gmail.com>
2024-09-28 21:13:15 +0330

    feat: add Products component for displaying product gallery

---
+++ pages/products/index.js
@@ -0,0 +1,11 @@
+import ProductGallery from "@/Pages/ProductGallery";
+
+const Products = () => {
+    return (
+        <div>
+            <ProductGallery />
+        </div>
+    );
+};
+
+export default Products;

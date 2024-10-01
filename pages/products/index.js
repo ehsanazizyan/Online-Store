dda029bc2df18052d6926ed8c48f092c4570c1aa
Ehsan Azizyan <ehsanazizyan74@gmail.com>
2024-10-01 15:22:04 +0330

    feat: add Navbar to Products page with scroll behavior

--- pages/products/index.js
+++ pages/products/index.js
@@ -1,10 +1,32 @@
+import Navbar from "@/Navbar";
 import ProductGallery from "@/Pages/ProductGallery";
+import { useEffect, useState } from "react";
 
 const Products = () => {
+    const [isScrolled, setIsScrolled] = useState(false);
+    useEffect(() => {
+        const handleScroll = () => {
+            const { scrollTop } = document.documentElement;
+            setIsScrolled(scrollTop > 0);
+        };
+
+        if (typeof window !== "undefined") {
+            window.addEventListener("scroll", handleScroll);
+            handleScroll();
+        }
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
     return (
-        <div>
+        <>
+            <div className={`top-0 z-50 w-full mb-4 ${isScrolled ? "fixed" : ""}`}>
+                <Navbar />
+            </div>
+
             <ProductGallery />
-        </div>
+        </>
     );
 };
 

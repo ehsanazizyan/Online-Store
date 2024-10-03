d7fbe09143ae20608c02a5da2833235c0d92e9b1
Ehsan Azizyan <ehsanazizyan74@gmail.com>
2024-10-03 09:22:42 +0330

    feat: add useProductId hook to fetch product details by ID
    - Created a custom hook using React Query to fetch product data from an API by product ID.
    - Added stale and cache times to optimize performance.

--- hooks/useProductid.js
+++ hooks/useProductid.js
@@ -1,5 +1,18 @@
-const fetchProductById = (id) => {
-    console.log(id);
+import { useQuery } from "@tanstack/react-query";
+
+const fetchProductById = async (id) => {
+    const response = await fetch(`https://dummyjson.com/products/${id}`);
+    if (!response.ok) {
+        throw new Error("Network response was not ok");
+    }
+    return response.json();
 };
-const useProduct = (productId) => {};
-export default useProduct;
+
+export const useProductId = (productId) => {
+    return useQuery({
+        queryKey: [`product-${productId}`],
+        queryFn: () => fetchProductById(productId),
+        staleTime: 60000, // 1 minute
+        cacheTime: 120000, // 2 minute
+    });
+};

d0d3c5db988cf557ee79d488a7c0104d7bd0b6a1
Ehsan Azizyan <ehsanazizyan74@gmail.com>
2024-10-12 15:48:51 +0330

    feat: improve back navigation with router.back() and enhance button layout
    - Replaced `Link` with `router.back()` for better back navigation.
    - Adjusted button layout for better alignment using flexbox.
    - Maintained the original layout for product content and image gallery.

--- components/ProductDetails/index.js
+++ components/ProductDetails/index.js
@@ -4,13 +4,20 @@ import PhotoGallery from "./PhotoGallery";
 import ContentLeft from "./ContentLeft";
 import Button from "@/Button";
 import { ChevronLeft } from "lucide-react";
+import { useRouter } from "next/router";
 
 const ProductDetails = ({ product }) => {
+    const router = useRouter();
     return (
         <div className="bg-base-100 p-6 md:p-6  rounded-lg">
-            <Link href="/products" className="text-right block">
-                <Button label={<ChevronLeft />} tooltip="Back to Products" />
-            </Link>
+            <div className="flex justify-end">
+                <Button
+                    label={<ChevronLeft />}
+                    tooltip="Back"
+                    className=""
+                    onClick={() => router.back()}
+                />
+            </div>
             <div className="flex flex-col grid-cols-12 gap-4 md:grid">
                 <div className="col-span-4 ">
                     <ContentLeft {...product} />

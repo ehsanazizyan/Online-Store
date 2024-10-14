70ab615528b1aff1f98697cad03818193447a25f
Ehsan Azizyan <ehsanazizyan74@gmail.com>
2024-10-14 19:13:18 +0330

    feat(Navbar): add cart item and total price display with improved conditional rendering
    - Integrated `cartStore` to display the total number of items and subtotal price in the cart.
    - Added conditional logic to hide the search input on the cart page and display only on other pages.
    - Enhanced mobile view with category list visibility only on the `/products` route.
    - Updated the cart dropdown with item and price details, along with a view cart button.

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,3 +1,5 @@
+import { cartStore } from "@/cartStore";
+
 import CategoryList from "@/Pages/CategoryList";
 import useSearchStore from "@/searchStore";
 import { useSearch } from "@/useSearch";
@@ -31,6 +33,8 @@ const Navbar = () => {
     const [openSearchInput, setOpenSearchInput] = useState(false);
     const debouncedSearch = useDebounce(search, 500);
 
+    const { totalItems, totalPrice } = cartStore();
+    //_________________________________________
     // Conditionally triggering the search request based on the debounced input and the current path:
     // 1. If `debouncedSearch` has a value and we are on the `/products` page (`asPath === "/products"`),
     //    the search query will be sent to fetch the search results.
@@ -38,12 +42,12 @@ const Navbar = () => {
     //    ensuring that the search functionality is only active on the products page.
     // 3. Additionally, debounce (with a 1-second delay) is used to avoid making server requests
     //    on every keystroke, only sending the request when the user pauses typing.
-
     const {
         data: searchResults,
         isError,
         isLoading,
     } = useSearch(debouncedSearch && asPath === "/products" ? debouncedSearch : "");
+    //_________________________________________
 
     const { setProducts, setMessage, setSearchStore } = useSearchStore();
     useEffect(() => {
@@ -83,7 +87,10 @@ const Navbar = () => {
                 </Link>
             </div>
             <>
-                <div className="flex items-center gap-1 ">
+                <div
+                    className={`flex items-center gap-1  ${
+                        asPath === "/cart" ? "hidden" : "block"
+                    }`}>
                     {isLoading && <span className="loading loading-spinner text-primary"></span>}
                     <input
                         value={search}
@@ -100,7 +107,7 @@ const Navbar = () => {
                     />
                 </div>
 
-                <div className={`md:hidden ${asPath === "/products" ? "block" : "hidden"}`}>
+                <div className={`z-50 md:hidden ${asPath === "/products" ? "block" : "hidden"}`}>
                     <ul className="menu menu-horizontal px-1">
                         <li>
                             <details>
@@ -121,7 +128,7 @@ const Navbar = () => {
                         <div className="indicator">
                             <svg
                                 xmlns="http://www.w3.org/2000/svg"
-                                className="h-5 w-5"
+                                className="h-5 w-5 md:h-6 md:w-6"
                                 fill="none"
                                 viewBox="0 0 24 24"
                                 stroke="currentColor">
@@ -132,15 +139,19 @@ const Navbar = () => {
                                     d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
                                 />
                             </svg>
-                            <span className="badge badge-sm indicator-item">8</span>
+                            <span className="badge badge-sm md:badge-md indicator-item text-primary">
+                                {totalItems}
+                            </span>
                         </div>
                     </div>
                     <div
                         tabIndex={0}
                         className="card card-compact dropdown-content bg-base-100 z-[1] mt-3 w-52 shadow">
                         <div className="card-body">
-                            <span className="text-lg font-bold">8 Items</span>
-                            <span className="text-info">Subtotal: $999</span>
+                            <span className="text-lg font-bold">{totalItems} Items</span>
+                            <span className="text-primary font-semibold">
+                                Subtotal: ${totalPrice}
+                            </span>
                             <div className="card-actions">
                                 <Link href="/cart" className="btn btn-primary btn-block">
                                     View cart

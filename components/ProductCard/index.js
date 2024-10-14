aaaef5828e833643ae20e0d12e3b5820b55bf0a2
Ehsan Azizyan <ehsanazizyan74@gmail.com>
2024-10-14 19:14:55 +0330

    feat(ProductCard): integrate ActionBtn and improve layout for better stock and discount display
    - Added `ActionBtn` component to handle product actions within the card.
    - Enhanced layout for stock display with conditional rendering for low stock items.
    - Improved mobile and desktop visibility for stock and discount information.
    - Refactored code structure to include the `product` prop for passing product details to `ActionBtn`.

--- components/ProductCard/index.js
+++ components/ProductCard/index.js
@@ -1,3 +1,4 @@
+import ActionBtn from "@/ActionBtn";
 import { Star } from "lucide-react";
 import Link from "next/link";
 
@@ -10,44 +11,48 @@ const ProductCard = ({
     title,
     thumbnail,
     discountPercentage,
+    product,
 }) => {
     return (
-        <Link href={`/products/${id}`}>
-            <div className="bg-base-100 shadow-xl flex md:flex-col items-center w-full md:w-80 font-semibold">
-                <div className="card-body w-full md:order-1 space-y-3 ">
-                    <h2 className="card-title text-sm md:text-base">{title}</h2>
+        <div className="bg-base-100 shadow-xl ">
+            <Link href={`/products/${id}`}>
+                <div className="flex md:flex-col items-center w-full md:w-80 font-semibold">
+                    <div className="card-body w-full md:order-1 space-y-3 ">
+                        <h2 className="card-title text-sm md:text-base">{title}</h2>
 
-                    <div className="flex justify-between items-center ">
-                        <span>$ {price} </span>
-                        <span className="bg-red-500 text-white px-2 rounded-lg text-sm">
-                            {discountPercentage} %
-                        </span>
-                    </div>
-                    <div className="flex justify-between items-center">
-                        <div className="flex items-center gap-1">
-                            <span>
-                                <Star size={16} strokeWidth={3} className="text-yellow-500" />
+                        <div className="flex justify-between items-center ">
+                            <span>$ {price} </span>
+                            <span className="bg-red-500 text-white px-2 rounded-lg text-sm">
+                                {discountPercentage} %
                             </span>
-                            <span> {rating}</span>
                         </div>
-                        {stock <= 3 && (
-                            <span className="text-red-500 min-w-max hidden md:block">
+                        <div className="flex justify-between items-center">
+                            <div className="flex items-center gap-1">
+                                <span>
+                                    <Star size={16} strokeWidth={3} className="text-yellow-500" />
+                                </span>
+                                <span> {rating}</span>
+                            </div>
+                            {stock <= 3 && (
+                                <span className="text-red-500 min-w-max hidden md:block">
+                                    Only {stock} of this product left
+                                </span>
+                            )}
+                        </div>
+
+                        {stock <= 5 && (
+                            <span className="text-red-500 text-sm block md:hidden">
                                 Only {stock} of this product left
                             </span>
                         )}
                     </div>
-
-                    {stock <= 5 && (
-                        <span className="text-red-500 text-sm block md:hidden">
-                            Only {stock} of this product left
-                        </span>
-                    )}
+                    <figure>
+                        <img src={thumbnail} alt={category} className="w-3/4 md:m-auto ml-auto" />
+                    </figure>
                 </div>
-                <figure>
-                    <img src={thumbnail} alt={category} className="w-3/4 md:m-auto ml-auto" />
-                </figure>
-            </div>
-        </Link>
+            </Link>
+            <ActionBtn product={product} id={id} />
+        </div>
     );
 };
 
